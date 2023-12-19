713fb23dcc0568b8d91988c8c26c55879195b336
tateoeng <tate.o.engstrand@gmail.com>
2023-12-19 12:34:11 -0700

    Update index.php
    delete now deletes empty parent directories.
    
    restamp copies all files, not just index.md.
    
    trailing newline issue is maybe fixed.

--- index.php
+++ index.php
@@ -7,34 +7,34 @@
 <?php
 include('Parsedown.php');
 
-// if (isset($_GET['submit'])) {
-    // print('<a href="./">$_GET[\'submit\'] = '.$_GET['submit'].'</a>');
-    // print('<hr />');
-// }
 
-if (isset($_GET['blog_timezone'])) {
-    date_default_timezone_set($_GET['blog_timezone']);
+
+
+
+if (isset($_POST['blog_timezone'])) {
+    date_default_timezone_set($_POST['blog_timezone']);
 }
 
 
 
 
 
-if (isset($_GET['submit']) && $_GET['submit'] == 'Set_up') {
+
+if (isset($_POST['submit']) && $_POST['submit'] == 'Set_up') {
     if (!file_exists('jot.ini')) {
         $file = fopen('jot.ini', 'w');
         // fwrite($file, 'Jot_loaded = "Jot.ini loaded"'.PHP_EOL);
         // fwrite($file, 'blog_number_posts = "3"'.PHP_EOL);
-        fwrite($file, 'blog_name = "'.$_GET['blog_name'].'"'.PHP_EOL);
-        fwrite($file, 'blog_tagline = "'.$_GET['blog_tagline'].'"'.PHP_EOL);
-        fwrite($file, 'blog_timezone = "'.$_GET['blog_timezone'].'"'.PHP_EOL);
-        fwrite($file, 'blog_timestamp = "'.$_GET['blog_timestamp'].'"'.PHP_EOL);
-        fwrite($file, 'link_timestamp = "'.$_GET['link_timestamp'].'"'.PHP_EOL);
-        fwrite($file, 'blog_nposts= "'.$_GET['blog_nposts'].'"'.PHP_EOL);
+        fwrite($file, 'blog_name = "'.$_POST['blog_name'].'"'.PHP_EOL);
+        fwrite($file, 'blog_tagline = "'.$_POST['blog_tagline'].'"'.PHP_EOL);
+        fwrite($file, 'blog_timezone = "'.$_POST['blog_timezone'].'"'.PHP_EOL);
+        fwrite($file, 'blog_timestamp = "'.$_POST['blog_timestamp'].'"'.PHP_EOL);
+        fwrite($file, 'link_timestamp = "'.$_POST['link_timestamp'].'"'.PHP_EOL);
+        fwrite($file, 'blog_nposts= "'.$_POST['blog_nposts'].'"'.PHP_EOL);
         fclose($file); 
     } else {
         $file = fopen('jot.ini', 'w');
-        fwrite($file, $_GET['body'].PHP_EOL);
+        fwrite($file, $_POST['body']);
         fclose($file);
     }
 }
@@ -43,9 +43,9 @@ if (isset($_GET['submit']) && $_GET['submit'] == 'Set_up') {
 
 
 
-if (isset($_GET['submit']) && $_GET['submit'] == 'Save_stylesheet') {
+if (isset($_POST['submit']) && $_POST['submit'] == 'Save_stylesheet') {
     $file = fopen('style.css', 'w');
-    fwrite($file, $_GET['body'].PHP_EOL);
+    fwrite($file, $_POST['body']);
     fclose($file);
 }
 
@@ -53,10 +53,10 @@ if (isset($_GET['submit']) && $_GET['submit'] == 'Save_stylesheet') {
 
 
 
-if (isset($_GET['submit']) && $_GET['submit'] == 'Preferences') {
+if (isset($_POST['submit']) && $_POST['submit'] == 'Preferences') {
     $ini = file_get_contents('jot.ini');
     echo '<div class="feature">
-<form action="./" method="get">
+<form action="./" method="post">
 <textarea name="body">'.$ini.'</textarea>
 <p>&nbsp;</p>
 <button type="submit" id="Set_up" name="submit" value="Set_up">Update</button>
@@ -68,10 +68,10 @@ if (isset($_GET['submit']) && $_GET['submit'] == 'Preferences') {
 
 
 
-if (isset($_GET['submit']) && $_GET['submit'] == 'Stylesheet') {
+if (isset($_POST['submit']) && $_POST['submit'] == 'Stylesheet') {
     $style = file_get_contents('style.css');
     echo '<div class="feature">
-<form action="./" method="get">
+<form action="./" method="post">
 <textarea name="body">'.$style.'</textarea>
 <br />
 <button type="submit" id="Save_stylesheet" name="submit" value="Save_stylesheet">Update</button>
@@ -83,8 +83,8 @@ if (isset($_GET['submit']) && $_GET['submit'] == 'Stylesheet') {
 
 
 
-if (isset($_GET['submit']) && $_GET['submit'] == 'New_post') {
-    echo '<form class="feature" action="./" method="get">
+if (isset($_POST['submit']) && $_POST['submit'] == 'New_post') {
+    echo '<form class="feature" action="./" method="post">
 <p><textarea name="body"></textarea></p>
 <p><button type="submit" id="Publish" name="submit" value="Publish">Publish</button></p>
 </form>';
@@ -94,14 +94,14 @@ if (isset($_GET['submit']) && $_GET['submit'] == 'New_post') {
 
 
 
-if (isset($_GET['submit']) && $_GET['submit'] == 'Publish') {
+if (isset($_POST['submit']) && $_POST['submit'] == 'Publish') {
     $ini = parse_ini_file('jot.ini');
     date_default_timezone_set($ini['blog_timezone']);
     $new_timestamp = date($ini['link_timestamp']);
     if (!is_dir($new_timestamp)) mkdir($new_timestamp, 0755, true);
     if (!file_exists($new_timestamp.'index.md')) {
         $file = fopen($new_timestamp.'/index.md', 'w');
-        fwrite($file, $_GET['body'].PHP_EOL);
+        fwrite($file, $_POST['body']);
         fclose($file); 
     } 
 }
@@ -110,20 +110,20 @@ if (isset($_GET['submit']) && $_GET['submit'] == 'Publish') {
 
 
 
-if (isset($_GET['submit']) && $_GET['submit'] == 'View') {
-    $post = file_get_contents($_GET['link'].'index.md');
+if (isset($_POST['submit']) && $_POST['submit'] == 'View') {
+    $post = file_get_contents($_POST['link'].'index.md');
     $Parsedown = new Parsedown();
     print('<div class="feature">');
     echo $Parsedown->text($post);
-    print($_GET['link']);
-    echo '<form action="./" method="get">
+    print($_POST['link']);
+    echo '<form action="./" method="post">
 <button type="submit" id="Edit" name="submit" value="Edit">Edit</button>
 <button type="submit" id="Duplicate" name="submit" value="Duplicate">Duplicate</button>
 <button type="submit" id="Delete" name="submit" value="Delete">Delete</button>
 <button type="submit" id="Restamp" name="submit" value="Restamp">Restamp</button>
 <input type="file" class="File_to_upload" name="the_file" />
 <input type="submit" class="Upload" name="submit" value="Upload" />
-<input type="hidden" id="Link" name="link" value="'.$_GET['link'].'" />
+<input type="hidden" id="Link" name="link" value="'.$_POST['link'].'" />
 </form>';
     print('</div>'.PHP_EOL);
 }
@@ -132,13 +132,13 @@ if (isset($_GET['submit']) && $_GET['submit'] == 'View') {
 
 
 
-if (isset($_GET['submit']) && $_GET['submit'] == 'Edit') {
-    $post = file_get_contents($_GET['link'].'index.md');
-    echo '<form action="./" method="get">
+if (isset($_POST['submit']) && $_POST['submit'] == 'Edit') {
+    $post = file_get_contents($_POST['link'].'index.md');
+    echo '<form action="./" method="post">
 <textarea name="body">'.$post.'</textarea>
 <br />
 <button type="submit" id="Save" name="submit" value="Save">Save</button>
-<input type="hidden" id="Link" name="link" value="'.$_GET['link'].'" />
+<input type="hidden" id="Link" name="link" value="'.$_POST['link'].'" />
 </form>
 <hr />';
 }
@@ -147,11 +147,11 @@ if (isset($_GET['submit']) && $_GET['submit'] == 'Edit') {
 
 
 
-if (isset($_GET['submit']) && $_GET['submit'] == 'Save') {
+if (isset($_POST['submit']) && $_POST['submit'] == 'Save') {
     $ini = parse_ini_file('jot.ini');
     date_default_timezone_set($ini['blog_timezone']);
-    $file = fopen($_GET['link'].'index.md', 'w');
-    fwrite($file, $_GET['body'].PHP_EOL);
+    $file = fopen($_POST['link'].'index.md', 'w');
+    fwrite($file, $_POST['body']);
     fclose($file);
 }
 
@@ -159,14 +159,14 @@ if (isset($_GET['submit']) && $_GET['submit'] == 'Save') {
 
 
 
-if (isset($_GET['submit']) && $_GET['submit'] == 'Duplicate') {
+if (isset($_POST['submit']) && $_POST['submit'] == 'Duplicate') {
     $ini = parse_ini_file('jot.ini');
     date_default_timezone_set($ini['blog_timezone']);
     $new_timestamp = date($ini['link_timestamp']);
     if (!is_dir($new_timestamp)) mkdir($new_timestamp, 0755, true);
     $file = fopen($new_timestamp.'/index.md', 'w');
-    $post = file_get_contents($_GET['link'].'index.md');
-    fwrite($file, $post.PHP_EOL);
+    $post = file_get_contents($_POST['link'].'index.md');
+    fwrite($file, $post);
     fclose($file); 
 }
 
@@ -174,44 +174,77 @@ if (isset($_GET['submit']) && $_GET['submit'] == 'Duplicate') {
 
 
 
-if (isset($_GET['submit']) && $_GET['submit'] == 'Delete') {
-    $files = glob($_GET['link'].'*');
-    foreach ($files as $file) {
-        unlink($file);
-    }
-    rmdir($_GET['link']);
+// if (isset($_POST['submit']) && $_POST['submit'] == 'Delete') {
+    // $files = glob($_POST['link'].'*');
+    // foreach ($files as $file) {
+        // unlink($file);
+    // }
+    // rmdir($_POST['link']);
+// }
+
+
+
+
+
+if (isset($_POST['submit']) && $_POST['submit'] == 'Delete') {
+    $link = $_POST['link'];
+    $files = glob($link.'*');
+    foreach ($files as $file) { unlink($file); }
+    $links = explode('/', $link);
+    array_pop($links);
+    $link = implode('/', $links);
+    $count = count(scandir($link));
+    while ($count == 2) {
+        rmdir($link);
+        $links = explode('/', $link);
+        array_pop($links);
+        $link = implode('/', $links);
+        $count = count(scandir($link));
+    }    
 }
 
 
 
 
 
-if (isset($_GET['submit']) && $_GET['submit'] == 'Restamp') {
+
+if (isset($_POST['submit']) && $_POST['submit'] == 'Restamp') {
     $ini = parse_ini_file('jot.ini');
     date_default_timezone_set($ini['blog_timezone']);
     $new_timestamp = date($ini['link_timestamp']);
     if (!is_dir($new_timestamp)) mkdir($new_timestamp, 0755, true);
-    $file = fopen($new_timestamp.'/index.md', 'w');
-    $post = file_get_contents($_GET['link'].'index.md');
-    fwrite($file, $post.PHP_EOL);
-    fclose($file); 
 
-    $files = glob($_GET['link'].'*');
+    $link = $_POST['link'];
+    $files = glob($link.'*');
     foreach ($files as $file) {
-        print($file.'<br>');
-        unlink($file);
+        $tmp = explode('/', $file);
+        $filename = array_pop($tmp);
+        print($new_timestamp.'/'.$filename);
+        copy($file, $new_timestamp.'/'.$filename);
     }
-    rmdir($_GET['link']);
+
+    foreach ($files as $file) { unlink($file); }
+    $links = explode('/', $link);
+    array_pop($links);
+    $link = implode('/', $links);
+    $count = count(scandir($link));
+    while ($count == 2) {
+        rmdir($link);
+        $links = explode('/', $link);
+        array_pop($links);
+        $link = implode('/', $links);
+        $count = count(scandir($link));
+    }    
 }
 
 
 
 
 
-if (isset($_GET['submit']) && $_GET['submit'] == 'Upload') {
+if (isset($_POST['submit']) && $_POST['submit'] == 'Upload') {
     // adapted from https://www.w3schools.com/php/php_file_upload.asp
-    print($_GET['link']);
-    $target_dir = $_GET['link'];
+    print($_POST['link']);
+    $target_dir = $_POST['link'];
     $target_file = $target_dir . basename($_FILES["File_to_upload"]["name"]);
     move_uploaded_file($_FILES["File_to_upload"]["tmp_name"], $target_file);
 }
@@ -220,7 +253,7 @@ if (isset($_GET['submit']) && $_GET['submit'] == 'Upload') {
 
 
 
-if (isset($_GET['submit']) && $_GET['submit'] == 'Show_archives') {
+if (isset($_POST['submit']) && $_POST['submit'] == 'Show_archives') {
     // etc.
 }
 
@@ -232,7 +265,7 @@ if (isset($_GET['submit']) && $_GET['submit'] == 'Show_archives') {
 if (!file_exists('jot.ini')) {
     echo '<h1>welcome to jot</h1>
 <p id="problem_p">please fill out the form below to configure your blog</p>
-<form action="./" method="get">
+<form action="./" method="post">
 <h3><b>what</b></h3>
 <p>blog name</p>
 <p><input type="text" name="blog_name" /></p>
@@ -266,7 +299,7 @@ if (!file_exists('jot.ini')) {
     $archives = glob($wildpath.'/index.md');
 	// if there are no archives, show text box with publish button.
     if (count($archives) == 0) {
-        echo '<form action="./" method="get">
+        echo '<form action="./" method="post">
 <p><textarea name="body"></textarea></p>
 <p><button type="submit" id="Publish" name="submit" value="Publish">Publish</button></p>
 </form>';
@@ -274,9 +307,9 @@ if (!file_exists('jot.ini')) {
 
 	// if archives exist, show the blog.
     } else {
-		echo '<div class="masthead"><div class="blog_name"><h1><a href="./">'.$ini['blog_name'].'</a></h1><h6 class="tagline">'.
+		echo '<div class="masthead"><div class="blog_name"><h1><a href="./">'.$ini['blog_name'].'</a></h1><h4 class="tagline">'.
 $ini['blog_tagline'].'</h6></div>
-<form action="./" method="get">
+<form action="./" method="post">
 <button type="submit" class="New_s" name="submit" value="New_post">New post...</button>
 <button type="submit" id="Preferences" name="submit" value="Preferences">Preferences...</button>
 <button type="submit" id="Stylesheet" name="submit" value="Stylesheet">Edit stylesheet...</button>
@@ -296,13 +329,13 @@ $ini['blog_tagline'].'</h6></div>
             print('<div class="timestamp">'.$link.'</div>');
             echo $Parsedown->text($post);
             print('<br>');
-            echo '<form action="./" method="get" enctype="multipart/form-data">
+            echo '<form action="./" method="post" enctype="multipart/form-data">
 <button type="submit" id="View" name="submit" value="View">View</button>
 <button type="submit" id="Edit" name="submit" value="Edit">Edit</button>
 <button type="submit" id="Duplicate" name="submit" value="Duplicate">Duplicate</button>
 <button type="submit" id="Delete" name="submit" value="Delete">Delete</button>
 <button type="submit" id="Restamp" name="submit" value="Restamp">Restamp</button>
-
+<br />
 <input type="file" class="File_to_upload" name="File_to_upload" />
 <input type="submit" class="Upload" name="submit" value="Upload" />
 
@@ -314,7 +347,7 @@ $ini['blog_tagline'].'</h6></div>
         print('<h2>Archives</h2><br />');
         foreach ($archives as $archive) {
             $file = fopen($archive.'index.md', 'r');
-            print('<form action="./" method="get">
+            print('<form action="./" method="post">
 <button type="submit" id="View" name="submit" value="View">'.$archive.'<br />'.fgets($file).'</button>
 <input type="hidden" id="Link" name="link" value="'.$archive.'" />
 </form>');
